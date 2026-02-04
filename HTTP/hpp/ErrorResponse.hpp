807f42ba29c144e63255e98268479f8bf9ffb5f4
Aliceparis <130312924+Aliceparis@users.noreply.github.com>
2026-02-04 17:53:40 +0100

    Delete HTTP directory

--- HTTP/hpp/ErrorResponse.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef ERRORRESPONSE_HPP
-#define ERRORRESPONSE_HPP
-
-#include "HTTP/hpp/HTTPResponse.hpp"
-#include "HTTP/hpp/HTTPUtils.hpp" 
-
-HTTPResponse buildErrorResponse(int statusCode);
-
-#endif
